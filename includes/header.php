df102d80479dd8fa341564e96dc69aec27b8e9a8
TheGameFreak720 <dj-entityofthedark@live.com>
2017-08-26 22:49:31 -0400

    Started PHP Blog site

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Welcome to PHP Lovers Blog</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/blog.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="nav">
+            <a class="nav-link active" href="blog.php">Home</a>
+            <a class="nav-link" href="posts.php">All Posts</a>
+        </nav>
+    </div>
+</div>
+
+<div class="blog-header">
+    <div class="container">
+        <div class="logo"><img src="img/small.png"></div>
+        <h1 class="blog-title">PHP Lovers Blog</h1>
+        <p class="lead blog-description">PHP News, Tutorials, Videos & more</p>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-sm-8 blog-main">
