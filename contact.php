d43204abf9b02a2bb96c286a2bd9a96298c46425
TheGameFreak720 <dj-entityofthedark@live.com>
2017-09-17 20:28:26 -0400

    Started PHP mailer and fixed a bit the code

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
-<body>
+<body data-spy="scroll" data-target=".navbar" data-offset="20">
     <!-- Preloader -->
     <div id="overlay">
         <div class="spinner"></div>
@@ -42,36 +42,37 @@
     </header>
 
     <!-- Contact Form -->
-        <div class="container">
-             <form action="<?= $_SERVER['PHP_SELF']; ?>">
+    <div class="container">
+        <form action="<?= $_SERVER['PHP_SELF']; ?>">
 
-                <label for="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="Your name.." required>
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." required>
 
-                <label for="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.." required>
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.." required>
 
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Your email.." required>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Your email.." required>
 
-                <label for="country">Country</label>
-                <select id="country" name="country">
-                    <option value="usa">USA</option>
-                    <option value="afghanistan">Afghanistan</option>
-                    <option value="albania">Albania</option>
-                </select>
+            <label for="country">Country</label>
+            <select id="country" name="country">
+                <option value="usa">USA</option>
+                <option value="afghanistan">Afghanistan</option>
+                <option value="albania">Albania</option>
+            </select>
 
-                <label for="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" required></textarea>
+            <label for="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" required></textarea>
 
-                <input type="submit" value="Submit">
+            <input type="submit" value="Submit">
 
-            </form>
-        </div>
-        <script type="text/javascript" src="js/preloader.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/jquery.easeScroll.js"></script>
-        <script type="text/javascript" src="js/mouseScroll.js"></script>
+        </form>
+    </div>
+    <p class="copyright">&copy CommitHub 2017</p>
+    <script type="text/javascript" src="js/preloader.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easeScroll.js"></script>
+    <script type="text/javascript" src="js/mouseScroll.js"></script>
 </body>
 </html>
