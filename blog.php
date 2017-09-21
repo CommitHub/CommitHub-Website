f678acc13a8f0d5e79e4d27308e1375ed0952e7c
TheGameFreak720 <dj-entityofthedark@live.com>
2017-09-20 23:23:55 -0400

    Added a few lines in PHP and confirmed PHP is working but can't connect to MYSQL

--- blog.php
+++ blog.php
@@ -4,7 +4,15 @@
 <?php
     //Create DB Object
     $db = new Database();
+
+    //Create Query
+    $query = "SELECT * FROM posts";
+
+    //Run Query
+    $posts = $db->select($query);
 ?>
+<?php echo phpversion(); ?>
+<?php if($posts) : ?>
     <div class="blog-post">
         <h2 class="blog-post-title">International PHP Conference</h2>
         <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
@@ -19,4 +27,8 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et convallis ante, in rhoncus lacus. Sed vehicula, tortor sed sodales rhoncus, libero lorem feugiat diam, posuere porta tellus risus non libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis metus felis, bibendum in tortor a, elementum tempor sapien. Aliquam rhoncus erat quis enim accumsan placerat. Pellentesque mi quam, accumsan et tellus eget, gravida iaculis ipsum. Nulla cursus nec nisi at luctus. Aenean eget commodo erat, sit amet elementum magna. Praesent at libero volutpat, porta purus eu, maximus erat. Maecenas tristique id lacus at sollicitudin. Mauris ut mollis est, et laoreet sem. Etiam vitae leo a orci pretium pretium.</p>
         <a class="readmore" href="post.php?id=1">Read More</a>
     </div><!-- /.blog-post -->
+
+<?php else: ?>
+    <p>There are no posts yet</p>
+<?php endif; ?>
 <?php include 'includes/footer.php'; ?>
