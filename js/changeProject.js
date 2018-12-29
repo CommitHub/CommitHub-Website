096af00fe70e39cdb20cca21360288e03b47d0e7
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-29 12:37:31 -0500

    added athena to the projects

--- js/changeProject.js
+++ js/changeProject.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
     ///Works
-    var pictureTag = $('#picture');
-    var titleTag = $('#title-pro');
-    var aboutTag = $('#about-p');
-    var coffeeTag = $('#coffee');
-    var tecTag = $('#tec-p');
-    var linkTag = $('#link');
-    var coffeeIcon = "<div class=\"col-xs-3 col-md-1\"> <img class=\"img-responsive coffee\" " +
+    const pictureTag = $('#picture');
+    const titleTag = $('#title-pro');
+    const aboutTag = $('#about-p');
+    const coffeeTag = $('#coffee');
+    const tecTag = $('#tec-p');
+    const linkTag = $('#link');
+    const coffeeIcon = "<div class=\"col-xs-3 col-md-1\"> <img class=\"img-responsive coffee\" " +
         "src=\"img/coffee.png\"> </div>";
 
     //Adds and Removes the animate class
     $.fn.extend({
         animateCss: function (animationName) {
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
             this.addClass('animated ' + animationName).one(animationEnd, function () {
                 $(this).removeClass('animated ' + animationName);
             });
@@ -21,37 +21,43 @@ $(document).ready(function() {
         }
     });
 
-    var sentinel = {
+    const athena = {
+        picture: 'img/fish.jpg',
+        title: 'Athena',
+        about: 'An open source public API that provides information about the animals in the ocean',
+        coffee: coffeeIcon + coffeeIcon,
+        tec: 'Node.js, Express, JavaScript',
+        link: 'https://github.com/TheGameFreak720/Athena'
+    };
+
+    const sentinel = {
         picture: 'img/sentinnel-logo-transparent.png',
         title: 'Sentinel Professional Services',
         about: 'A portfolio website for a security services company',
         coffee: coffeeIcon,
-        technology: "Technology",
         tec: "HTML, CSS , JavaScript",
-        link: 'http://sentinelprofessional.com/'
+        link: 'https://thegamefreak720.github.io/sentinnel-professional-services/'
     };
 
-    var micro = {
+    const micro = {
         picture: 'img/Medium_without word.png',
         title: 'Micro.bot',
         about: 'An open source marketing twitter bot that helps you schedule post to twitter to engage your audience',
         coffee: coffeeIcon + coffeeIcon,
-        technology: "Technology",
         tec: "Pug, Node.js, MongoDB, jQuery, Passport.JS, CSS, JavaScript, HTML",
         link: 'https://github.com/TheGameFreak720/Micro.bot'
     };
 
-    var luvcheck = {
+    const luvcheck = {
         picture: 'img/LuvCheckFinal.png',
         title: 'Luvcheck Voice Assistant',
         about: "A voice assistant enabling the luvcheck application to be completely voice controlled and packed with nice features like tell you the weather, track your appointments, tell you random facts and more",
         coffee: coffeeIcon + coffeeIcon + coffeeIcon,
-        technology: "Technology",
         tec: "Annyang, ResponsiveVoice.JS, OpenWeather API, Wikipedia API, HTML, CSS, JavaScript, Node.JS",
         link: 'http://www.luvcheck.com/'
     };
 
-    var nmfm = {
+    const nmfm = {
         picture: "img/nmfm.jpg",
         title: "NMFM Accounting",
         about: "A portfolio/blog site made for an accountant in P.R. to make her business stand out",
@@ -60,7 +66,7 @@ $(document).ready(function() {
         link: 'http://nmfm-accountant.com'
     };
 
-    var zanra = {
+    const zanra = {
         picture: "img/zanra.PNG",
         title: "Zanra Consulting",
         about: "A portfolio/blog site made for a freelance marketing company",
@@ -69,20 +75,19 @@ $(document).ready(function() {
         link: 'http://zanraconsulting.com/'
     };
 
-    var defender = {
+    const defender = {
         picture: 'img/windowsdefender.png',
         title: 'Windows Defender',
         about: 'An arcade bullet hell game made in GameMaker Studio where you fight viruses infesting your windows computer',
         coffee: coffeeIcon + coffeeIcon,
-        technology: "Technology",
         tec: "GameMaker Studio, GML, Pyxel Edit",
-        link: 'https://nofile.io/f/e7enVw4Agd8/Windows+Defender.zip'
+        link: 'https://drive.google.com/open?id=0BxjhDKzUql56RHRyN1gtUk4xcjg'
     };
 
 
 
     //Holds all the projects objects
-    var projects = [sentinel, micro, luvcheck, nmfm, zanra, defender];
+    var projects = [athena, sentinel, micro, luvcheck, nmfm, zanra, defender];
 
     //Most Recent Project
     pictureTag.attr('src', projects[0].picture);
